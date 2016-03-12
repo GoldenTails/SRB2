917b3cd884a163c3ca4506517cb6a4f1eedc76da
Yukita Mayako <catgirl@goddess.moe>
2016-03-11 22:19:48 -0500

    Cleaned up and set NEWTICRATERATIO back to 1
    This makes the branch safe to merge.

--- src/doomdef.h
+++ src/doomdef.h
@@ -389,7 +389,7 @@ extern skincolor_t skincolors[MAXSKINCOLORS];
 // State updates, number of tics / second.
 // NOTE: used to setup the timer rate, see I_StartupTimer().
 #define TICRATE 35
-#define NEWTICRATERATIO 2 // try 4 for 140 fps :)
+#define NEWTICRATERATIO 1 // try 4 for 140 fps :)
 #define NEWTICRATE (TICRATE*NEWTICRATERATIO)
 
 #define MUSICRATE 1000 // sound timing is calculated by milliseconds
