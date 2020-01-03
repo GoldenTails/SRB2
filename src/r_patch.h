25395eec3dc8f3c05f79576b264fd9f659b701b6
Jaime Passos <lazymyuutsu@gmail.com>
2020-01-03 13:30:23 -0300

    Refactor ALL of the texture cache code before I go insane

--- src/r_patch.h
+++ src/r_patch.h
@@ -17,29 +17,6 @@
 #include "r_defs.h"
 #include "doomdef.h"
 
-#ifdef POLYRENDERER
-typedef struct
-{
-	INT16 width;
-	INT16 height;
-	UINT8 *data;
-} rsp_texture_t;
-
-// md2
-typedef struct
-{
-	INT16 width;
-	INT16 height;
-	void *data;
-} rsp_modeltexture_t;
-
-void RSP_GenerateTexture(patch_t *patch, UINT8 *buffer,
-						INT32 x, INT32 y,
-						INT32 maxwidth, INT32 maxheight,
-						boolean flip,
-						UINT8 *colormap, UINT8 *translation);
-#endif
-
 // Structs
 typedef enum
 {
@@ -60,6 +37,15 @@ typedef struct
 	boolean available;
 } spriteinfo_t;
 
+#ifdef POLYRENDERER
+typedef struct
+{
+	INT16 width;
+	INT16 height;
+	UINT8 *data;
+} rsp_texture_t;
+#endif
+
 // Conversions between patches / flats / textures...
 boolean R_CheckIfPatch(lumpnum_t lump);
 void R_TextureToFlat(size_t tex, UINT8 *flat);
@@ -67,6 +53,7 @@ void R_PatchToFlat(patch_t *patch, UINT8 *flat);
 void R_PatchToMaskedFlat(patch_t *patch, UINT16 *raw, boolean flip);
 patch_t *R_FlatToPatch(UINT8 *raw, UINT16 width, UINT16 height, UINT16 leftoffset, UINT16 topoffset, size_t *destsize, boolean transparency);
 patch_t *R_MaskedFlatToPatch(UINT16 *raw, UINT16 width, UINT16 height, UINT16 leftoffset, UINT16 topoffset, size_t *destsize);
+void R_GenerateSpriteTexture(patch_t *patch, UINT8 *buffer, INT32 x, INT32 y, INT32 maxwidth, INT32 maxheight, boolean flip, UINT8 *colormap, UINT8 *translation);
 
 // Portable Network Graphics
 boolean R_IsLumpPNG(const UINT8 *d, size_t s);
--- src/r_things.c
+++ src/r_things.c
@@ -90,7 +90,7 @@ static const char *spritename;
 // ==========================================================================
 
 //
-//
+// R_InstallSpriteLump
 //
 static void R_InstallSpriteLump(UINT16 wad,            // graphics patch
                                 UINT16 lump,
@@ -113,8 +113,14 @@ static void R_InstallSpriteLump(UINT16 wad,            // graphics patch
 	{
 		UINT8 rot = (rotation != 0) ? (rotation-1) : 0;
 		patch_t *patch;
-		rsp_spritetexture_t *tex = &sprtemp[frame].rsp_texture[rot];
+		rsp_spritetexture_t *tex;
 		INT32 blockwidth, blockheight;
+
+		lumpcache_t *lumpcache = wadfiles[wad]->patchcache->rspcache;
+		if (!lumpcache[lump])
+			Z_Malloc(sizeof(rsp_spritetexture_t) * 8, PU_SOFTPOLY, &lumpcache[lump]);
+		tex = lumpcache[lump];
+		tex += rot;
 
 		if (R_CheckIfPatch(lumppat))
 		{
@@ -167,10 +173,6 @@ static void R_InstallSpriteLump(UINT16 wad,            // graphics patch
 		{
 			sprtemp[frame].lumppat[r] = lumppat;
 			sprtemp[frame].lumpid[r] = lumpid;
-#ifdef POLYRENDERER
-			if (r > 0)
-				sprtemp[frame].rsp_texture[r] = sprtemp[frame].rsp_texture[0];
-#endif
 		}
 		sprtemp[frame].flip = flipped ? 0xFFFF : 0; // 1111111111111111 in binary
 		return;
