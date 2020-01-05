4674e4b9835962df54ce7127ff433f21f641658e
Jaime Passos <lazymyuutsu@gmail.com>
2020-01-05 03:32:15 -0300

    Fix another warning

--- src/r_things.c
+++ src/r_things.c
@@ -1391,7 +1391,9 @@ static void R_ProjectSprite(mobj_t *thing)
 
 	boolean model = false;
 	skin_t *skin;
+#ifdef POLYRENDERER
 	modelinfo_t *md2;
+#endif
 
 	INT32 x1, x2;
 	INT32 projx1, projx2;
@@ -1445,9 +1447,8 @@ static void R_ProjectSprite(mobj_t *thing)
 
 	// Lactozilla: Polygon renderer
 	skin = (skin_t *)thing->skin;
-	md2 = Model_IsAvailable(thing->sprite, skin);
-
 #ifdef POLYRENDERER
+	md2 = Model_IsAvailable(thing->sprite, skin);
 	model = (polyrenderer && cv_models.value && md2);
 	frustumclipping = false; // DONOTCULL turns this on.
 
@@ -1910,6 +1911,7 @@ static void R_ProjectSprite(mobj_t *thing)
 	vis->spritenum = thing->sprite;
 	vis->skin = thing->skin;
 	vis->model = model;
+
 #ifdef POLYRENDERER
 	if (model)
 		modelinview = true;
@@ -2139,6 +2141,7 @@ static void R_ProjectPrecipitationSprite(precipmobj_t *thing)
 	vis->shear.offset = 0;
 	vis->model = false;
 	vis->dontdrawsprite = false;
+	vis->skin = NULL;
 
 	vis->x1 = x1 < portalclipstart ? portalclipstart : x1;
 	vis->x2 = x2 >= portalclipend ? portalclipend-1 : x2;
