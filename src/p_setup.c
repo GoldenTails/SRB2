72233083d62a4696dd5222af212d15e85c974426
Jaime Passos <lazymyuutsu@gmail.com>
2020-01-05 17:21:50 -0300

    Rename functions

--- src/p_setup.c
+++ src/p_setup.c
@@ -3942,7 +3942,7 @@ boolean P_AddWadFile(const char *wadfilename)
 		CONS_Printf(M_GetText("No maps added\n"));
 
 	R_LoadSpriteInfoLumps(wadnum, numlumps);
-	R_ReloadModelSettings();
+	Model_ReloadSettings();
 
 	// reload status bar (warning should have valid player!)
 	if (gamestate == GS_LEVEL)
