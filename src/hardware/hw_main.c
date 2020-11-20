cbcfc9ce0f96b97db00fca32b4bbfdc146da274c
GoldenTails <milestailsprower101n2@gmail.com>
2020-11-20 00:14:25 -0600

    Fix warnings

--- src/hardware/hw_main.c
+++ src/hardware/hw_main.c
@@ -5402,6 +5402,8 @@ static void HWR_ProjectPrecipitationSprite(precipmobj_t *thing)
 	unsigned rot = 0;
 	UINT8 flip;
 
+	fixed_t interpx, interpy, interpz;
+
 	if (!thing)
 		return;
 
@@ -5413,9 +5415,9 @@ static void HWR_ProjectPrecipitationSprite(precipmobj_t *thing)
 	}
 
 	// uncapped/interpolation
-	fixed_t interpx = thing->x;
-	fixed_t interpy = thing->y;
-	fixed_t interpz = thing->z;
+	interpx = thing->x;
+	interpy = thing->y;
+	interpz = thing->z;
 
 	// do interpolation
 	if (cv_frameinterpolation.value == 1 && !paused)
