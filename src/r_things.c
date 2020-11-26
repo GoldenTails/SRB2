0b75fceb4b09522c0cdfd908a2cf65aae65d1941
GoldenTails <milestailsprower101n2@gmail.com>
2020-11-25 22:02:40 -0600

    Fix pausing causing stuttering due to interpolation

--- src/r_things.c
+++ src/r_things.c
@@ -1467,7 +1467,7 @@ static void R_ProjectSprite(mobj_t *thing)
 	if (thing->player) interpangle = thing->player->drawangle;
 
 	// do interpolation
-	if (cv_frameinterpolation.value == 1 && !paused)
+	if (cv_frameinterpolation.value == 1)
 	{
 		interpx = thing->old_x + FixedMul(rendertimefrac, thing->x - thing->old_x);
 		interpy = thing->old_y + FixedMul(rendertimefrac, thing->y - thing->old_y);
@@ -1787,7 +1787,7 @@ static void R_ProjectSprite(mobj_t *thing)
 		fixed_t linkscale;
 
 		thing = thing->tracer;
-		if (cv_frameinterpolation.value == 1 && !paused)
+		if (cv_frameinterpolation.value == 1)
 		{
 			interpx = thing->old_x + FixedMul(thing->x - thing->old_x, rendertimefrac);
 			interpy = thing->old_y + FixedMul(thing->y - thing->old_y, rendertimefrac);
@@ -2140,7 +2140,7 @@ static void R_ProjectPrecipitationSprite(precipmobj_t *thing)
 	fixed_t interpz = thing->z;
 
 	// do interpolation
-	if (cv_frameinterpolation.value == 1 && !paused)
+	if (cv_frameinterpolation.value == 1)
 	{
 		interpx = thing->old_x + FixedMul(rendertimefrac, thing->x - thing->old_x);
 		interpy = thing->old_y + FixedMul(rendertimefrac, thing->y - thing->old_y);
