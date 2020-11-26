9ca858f35c81f5b1919b487efc273be748053e9b
GoldenTails <milestailsprower101n2@gmail.com>
2020-11-26 15:51:07 -0600

    Have I_GetTimeFrac use I_GetPreciseTime to fix choppiness in Windows.

--- src/d_clisrv.c
+++ src/d_clisrv.c
@@ -5272,7 +5272,8 @@ void TryRunTics(tic_t realtics)
 				if (update_stats)
 					PS_START_TIMING(ps_tictime);
 
-				prev_tics = I_GetTime();
+				I_SetInterpolationDiff();
+
 				G_Ticker((gametic % NEWTICRATERATIO) == 0);
 				ExtraDataTicker();
 				gametic++;
